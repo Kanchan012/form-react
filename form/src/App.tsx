7a075a4abd7c925fbb95dde21038761da419ec49
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-01-14 13:33:35 +0545

    first commit

---
+++ form/src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'  
+
+function App() {
+  // const [count, setCount] = useState(0)
+  return (
+    <>
+      <div>
+        <h1>Simple Form</h1>
+        <label htmlFor="Description">Description</label>
+        <input type="text" placeholder='Enter your text' />
+        
+      </div>
+    </>
+  )
+}
+
+export default App
